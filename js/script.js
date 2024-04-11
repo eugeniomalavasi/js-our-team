1cc567c99f0e1a877c91decbd9b14529ad8badc3
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-04-11 14:47:57 +0200

    feat: object array created

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const teamData = [
+    {
+        Name = Wayne Barnett,
+        Role = Founder & CEO,
+        img = wayne-barnett-founder-ceo.jpg,
+    }
+    {
+        Name = Angela Caroll,
+        Role = Chief Editor,
+        img = angela-caroll-chief-editor.jpg,
+    }
+    {
+        Name = Walter Gordon,
+        Role = Office Manager,
+        img = walter-gordon-office-manager.jpg,
+    }
+    {
+        Name = Angela Lopez,
+        Role = Social Media Manager,
+        img = angela-lopez-social-media-manager.jpg,
+    }
+    {
+        Name = Scott Estrada,
+        Role = Developer,
+        img = scott-estrada-developer.jpg,
+    }
+    {
+        Name = Barbara Ramos,
+        Role = Graphic Designer,
+        img = barbara-ramos-graphic-designer.jpg,
+    }
+]
